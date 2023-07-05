ca80f44193a12e238c042b6d6bb3c00708697a10
MinseoHwang <seehm9215@gmail.com>
2023-07-05 21:06:35 +0900

    Creat DP and 주요소.py
    백준 DP문제 몇 개와 주유소 문제 풀이

--- 황민서/그리디 알고리즘/잃어버린 괄호(1541)/잃어버린괄호.py
+++ 황민서/그리디 알고리즘/잃어버린 괄호(1541)/잃어버린괄호.py
@@ -2,16 +2,14 @@
 
 #입력 및 변수설정
 import sys
-input_str = sys.stdin.readline().strip('\n')
-plus = input_str.split('-')
+plus = input().split('-')
 num = []
 
 #구현 
 for i in plus: # -를 기준으로 나눈 요소들 중 문자열 안에 +가 포함되어 있는 요소들의 덧셈 작업을 위해 for문을 사용하였다.
     temp = i.split('+')
     s = 0
-    for j in temp:
-        s += int(j)
+    s = sum(s)
     num.append(s)
     s = 0
 # 30 - 20 + 30 - 10 + 30 일 경우 num의 요소들은 [30, 50, 40]이 될 것이다.
