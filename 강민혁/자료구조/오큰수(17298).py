707202654e1324e2344f24285fb58dd09ff077a4
Paul Kang <48120607+kminh1209@users.noreply.github.com>
2023-08-11 21:50:52 +0900

    시간 초과 문제 해결
    - 반복문 최소화

--- 강민혁/자료구조/오큰수(17298).py
+++ 강민혁/자료구조/오큰수(17298).py
@@ -3,21 +3,19 @@ A = [3,5,2,7] 인 경우
 3의 오른족에 있으면서 3보다 큰 수 중 가장 왼쪽에 있는 원소 5가 오큰수
 5의 오른쪽에 있으면서 5보다 큰 수 중 가장 왼쪽에 있는 원소 7이 오큰수
 '''
+import sys
+n = int(input())
+A = list(map(int, sys.stdin.readline().split()))
+answer = [-1] * n
+stack = []
 
-N = int(input())
-A = list(map(int,input().split())) # 수열 입력
-Stack = []
-Answer = []
 
-for i in range(N):
-    for j in range(i+1,N):
-        if A[i] < A[j]:     # 오른쪽에서 큰수
-            Stack.append(A[j])
+stack.append(0)
+for i in range(1, n):
+    while stack and A[stack[-1]] < A[i]:
+        # i가 3일때 Stack에 남아있던 1이 남아 A[1] < A[3] 이 비교되어 answer[1]이 변경된다.
+        answer[stack.pop()] = A[i]
+    stack.append(i)
 
-    if Stack:   # Stack이 비어있지 않으면
-        Answer.append(Stack[0])
-    else:           # Stack이 비어있을 때
-        Answer.append(-1)
-    Stack.clear()
 
-print(" ".join(map(str,Answer))[:])
+print(*answer) # *answer = answer에 모든 원소들
